dd696f47d2774d131e798ce072b0bedb27acda8f
Vulnpire <89225495+Vulnpire@users.noreply.github.com>
2024-05-25 15:30:38 +0200

    Update 403.sh

--- 403.sh
+++ 403.sh
@@ -5,5 +5,5 @@ do
     url=$(echo $line | awk '{print $1}')
     header=$(echo $line | awk '{print $2}')
 
-    403 --url "$url" --header
+    403 --url "$url" --header > 403_check.txt
 done < 403.txt
